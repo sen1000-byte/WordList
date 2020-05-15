2b7f86eeca2244dfd8a1723921169ecdecd10084
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-15 21:10:44 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Chihiro Nishiwaki on 2020/05/15.
+//  Copyright © 2020 nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
